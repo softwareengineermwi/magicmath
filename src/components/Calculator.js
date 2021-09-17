c980ab0a7157161ae51c9e54498779f56a4105b5
Mwila Kaunda <mbkcust@gmail.com>
2021-09-17 11:40:24 +0200

    Hook it up

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Output from './Output';
 import Input from './Input';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: null,
-      total: null,
-    };
-    this.handler = this.handler.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handler(e) {
-    this.setState((state) => calculate(state, e.target.innerText));
-  }
+  const handleClick = (e) => {
+    setState({ ...state, ...calculate(state, e.target.innerText) });
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="grid container max-width-sm">
-        <Output value={next || (total || '0')} />
-        <Input handler={this.handler} />
-      </div>
-    );
-  }
-}
+  const { next, total } = state;
+
+  return (
+    <div className="grid container max-width-sm">
+      <Output value={next || total || '0'} />
+      <Input handler={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;
